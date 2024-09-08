950249ffbd8751d6b8bfb2de875c9f26d8dae458
NoahAkkad <noah.akkad@gmail.com>
2024-09-08 11:43:58 +0200

    First commit

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not found!</h1>
+      <p>Try going back to the previous page, and try to found anther Meal.</p>
+    </main>
+  );
+}
